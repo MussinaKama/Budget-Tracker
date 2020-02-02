de91d548b56406db415a82a0fcb14d5440bd86d5
Kamshat Mussina <mussinak777@gmail.com>
2020-02-01 20:33:09 -0700

    added service-workers, webmanifest

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+const Transaction = require("../models/transaction.js");
+
+module.exports = function (app) {
+app.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+}
+
